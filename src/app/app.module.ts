6ee48e46a93c77d3bdc4fa79d6d3ad5df2871785
mirulnorazmi <mmiirul00@gmail.com>
2024-11-05 00:18:16 +0800

    fix bug

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,19 @@ import { QRCodeModule } from 'angularx-qrcode';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { StaffService } from './CORE/services/staff/staff.service';
+import { provideHttpClient, withFetch } from '@angular/common/http';
+import {
+  BrowserModule,
+  provideClientHydration,
+} from '@angular/platform-browser';
 
 @NgModule({
   declarations: [],
-  imports: [
-    CommonModule,
-    QRCodeModule ,
-    FormsModule,
-    HttpClientModule,
+  imports: [CommonModule, QRCodeModule, FormsModule, HttpClientModule],
+  providers: [
+    StaffService,
+    provideClientHydration(),
+    provideHttpClient(withFetch()),
   ],
-  providers: [StaffService],
 })
-export class AppModule { }
+export class AppModule {}
